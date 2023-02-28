9b91534c7cf80513f55694d28ca28b72d6ad1e60
bonben365 <bonben365@gmail.com>
2023-02-28 07:17:09 +0700

    Update ExchangeOnline.ps1

--- ExchangeOnline.ps1
+++ ExchangeOnline.ps1
@@ -3,7 +3,8 @@ if($Module.count -eq 0) {
      Write-Host 'Required Exchange OnlineEXO module is not available' -ForegroundColor yellow 
 $Confirm= Read-Host 'Are you sure you want to install module?' [Y] Yes [N] No
 if($Confirm -match "[yY]") {
-     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -ErrorAction:SilentlyContinue
+     #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -ErrorAction:SilentlyContinue
+     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
      Install-PackageProvider -Name NuGet -Force
      Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
      Install-Module -Name ExchangeOnlineManagement -Force
